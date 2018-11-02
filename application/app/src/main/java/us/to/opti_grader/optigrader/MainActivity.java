709c9ffdbec209b70b4ad5a5fe9e2ace6e9febf9
ventusboy13 <ventusboy13@gmail.com>
2018-11-02 14:29:02 -0400

    Transferred app files

--- application/app/src/main/java/us/to/opti_grader/optigrader/MainActivity.java
+++ application/app/src/main/java/us/to/opti_grader/optigrader/MainActivity.java
@@ -1,7 +1,10 @@
 package us.to.opti_grader.optigrader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.enter_id);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 }
