0775d5bef6bb95a34df80d11abdb72bd150fc768
ventusboy13 <ventusboy13@gmail.com>
2018-11-09 13:52:16 -0500

    Transferred some app files

--- application/app/src/main/java/us/to/opti_grader/optigrader/MainActivity.java
+++ application/app/src/main/java/us/to/opti_grader/optigrader/MainActivity.java
@@ -5,14 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.i("opencv","OpenCV initialization successful");
+        } else {
+            Log.i("opencv","OpenCV initialization failed");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
@@ -21,5 +33,16 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+    public void CameraActivity(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.enter_id);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+
+
+        // I dont have the openCV stuff but this should start it
+        startActivity(intent);
+    }
+
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 }
