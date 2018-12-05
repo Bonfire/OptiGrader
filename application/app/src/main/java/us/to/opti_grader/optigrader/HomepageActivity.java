eeaab49fdb058c995e0d1ad9affbdcff3d00b7a6
mikal-pc <ventusboy13@gmail.com>
2018-12-04 22:53:14 -0500

    created homepage and login functionality

---
+++ application/app/src/main/java/us/to/opti_grader/optigrader/HomepageActivity.java
@@ -0,0 +1,38 @@
+package us.to.opti_grader.optigrader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import us.to.optigrader.optigrader.R;
+
+public class HomepageActivity extends AppCompatActivity {
+    private SessionHandler session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        session = new SessionHandler(getApplicationContext());
+        user user = session.getUserDetails();
+        TextView welcomeText = findViewById(R.id.welcomeText);
+
+        welcomeText.setText("Welcome "+user.getFullName()+", your session will expire on "+user.getSessionExpiryDate());
+
+        Button logoutBtn = findViewById(R.id.logoutBtn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                session.logoutUser();
+                Intent i = new Intent(HomepageActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+    }
+}
